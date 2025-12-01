955c6d97fa7e567741660f5bf04a83a2c5f73fe5
Arshad <arshad@MacBook-Pro-Arshad.local>
2025-12-01 14:55:50 +0700

    maintenance

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -5,6 +5,7 @@ import { useAuthStore } from "@/lib/types";
 import { AdminProfileView } from "@/components/profile/profile-admin-page";
 import { ProfileSkeleton } from "@/components/profile/profile-skeleton";
 import { UserProfileView } from "@/components/profile/profile-user-page";
+import { MaintenanceForm } from "@/components/login/maintenance-page";
 
 
 export default function ProfilePage() {
@@ -20,8 +21,10 @@ export default function ProfilePage() {
   }
 
   if (role === "Superuser" || role === "Admin") {
-    return <AdminProfileView />;
+    // return <AdminProfileView />;
+    return <MaintenanceForm />;
   } else {
-    return <UserProfileView />;
+    return <MaintenanceForm />;
+    // return <UserProfileView />;
   }
 }
--- app/vendors/page.tsx
+++ app/vendors/page.tsx
@@ -1,6 +1,7 @@
 
+import { MaintenanceForm } from "@/components/login/maintenance-page";
 import { VendorPage } from "@/components/vendors/vendor-page";
 
 export default function MaterialsPage() {
-  return <VendorPage />;
+  return <MaintenanceForm />;
 }
